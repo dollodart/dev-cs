465b5ccf0492e36f67efd85a5d949932278a1e62
david <davidollodart@gmail.com>
2020-02-16 21:33:04 -0800

    colors file

---
+++ colors.py
@@ -0,0 +1,8 @@
+from pyx import pyxcolor
+
+devcolors = {'ntype':pyxcolor.rgb.red
+        ,'gate':pyxcolor.rgb(0.4,0.4,0.4)
+        ,'ptype': pyxcolor.rgb(0.5,0.5,0)
+        ,'oxide':pyxcolor.rgb.black
+        ,'silicon':pyxcolor.rgb.blue
+        }
